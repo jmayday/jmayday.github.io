046cce28538c960a298632c24023acd5b9d3abb1
jmayday <jmayday>
2017-03-07 11:44:46 +0100

    java-junit5 Add junit4 non-nested example

--- java-junit5/examples/ju5-gradle/src/test/java/com/pgssoftware/nested/DescriptiveNamingTest.java
+++ java-junit5/examples/ju5-gradle/src/test/java/com/pgssoftware/nested/DescriptiveNamingTest.java
@@ -1,5 +1,29 @@
 package com.pgssoftware.nested;
 
 
+import org.junit.Test;
+
 public class DescriptiveNamingTest {
+
+    @Test
+    public void shouldReturnUniqueCombinationsWhenInputIsCorrect() {
+
+    }
+    @Test
+    public void shouldReturnSameResultsForSameParametersOnMultipleCalls() {
+
+    }
+    @Test
+    public void shouldBeAbleToStartAtGivenSequence() {
+
+    }
+    @Test
+    public void shouldHandleNullsWhenInputIsIncorrect() {
+
+    }
+    @Test
+    public void shouldProduceVerboseOutputWhenInputIsIncorrect() {
+
+    }
+
 }
